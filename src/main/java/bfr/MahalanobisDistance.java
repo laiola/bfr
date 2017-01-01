bd16dbf649cc5c7aeb7946bdb7f0545913e69382
laiola <laiola1003@gmail.com>
2017-01-01 16:18:59 +0300

    Update distance

--- src/main/java/bfr/MahalanobisDistance.java
+++ src/main/java/bfr/MahalanobisDistance.java
@@ -10,35 +10,37 @@ import java.util.ArrayList;
 public class MahalanobisDistance {
 
     public static double calculate(Cluster ds, Vector vector) {
-        double sigma = 0;
+        double distance = 0;
         ArrayList<Double> coords = vector.getCoordinates();
+        ArrayList<Double> centroid = ds.getCentroid().getCoordinates();
+
         int length = coords.size();
         SufficientStatistic statistic = ds.getStatistic();
-        int number = statistic.getN();
+        int number = statistic.getN() + 1;
         double[] sum = statistic.getSum();
         double[] sumsq = statistic.getSumsq();
 
         for (int i = 0; i < length; i++) {
-            sigma += (sumsq[i] + Math.pow(coords.get(i), 2) )/ number - Math.pow(((sum[i] + coords.get(i)) / number), 2);
+            double sigma = (sumsq[i] + Math.pow(coords.get(i), 2) )/ number
+                    - Math.pow(((sum[i] + coords.get(i)) / number), 2);
+            distance += Math.pow(((coords.get(i) - centroid.get(i)) / Math.sqrt(sigma)), 2);
         }
-        return Math.sqrt(sigma);
+        return Math.sqrt(distance);
     }
 
     public static double calculate(Vector vector1, Vector vector2) {
-        double sigma = 0;
         ArrayList<Double> coords1 = vector1.getCoordinates();
         ArrayList<Double> coords2 = vector2.getCoordinates();
+        double distance = 0;
 
         int length = coords1.size();
 
         for (int i = 0; i < length; i++) {
-            sigma += (Math.pow(coords1.get(i), 2) + Math.pow(coords2.get(i), 2)) / 2
+            double sigma = (Math.pow(coords1.get(i), 2) + Math.pow(coords2.get(i), 2)) / 2
                     - Math.pow(((coords1.get(i) + coords2.get(i)) / 2), 2);
-
-            //sigma += Math.pow(coords1.get(i) - coords2.get(i), 2);
+            distance += Math.pow(((coords1.get(i) - coords2.get(i)) / Math.sqrt(sigma)), 2);
         }
-        //System.out.println(Math.sqrt(sigma));
-        return Math.sqrt(sigma);
+        return Math.sqrt(distance);
     }
 
 }
